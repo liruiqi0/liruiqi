d57ceb33d8137c7cbfa0ad48a357484fd452bf55
LRQ <LRQ@DESKTOP-15ML4V7>
2018-12-14 11:48:04 +0800

    提交GitHub测试1

---
+++ src/main/java/com/example/demo/ZzdemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZzdemoApplication.class, args);
+	}
+
+}
+
